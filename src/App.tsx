1718802d32c8742b5429a283a01295df31fb224e
mahegamovers <mahegamoversinvestmentscompany@gmail.com>
2026-07-07 22:03:55 +0300

    Refactor App component and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,93 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Header from './components/Header'
+import Header from "./components/Header";
 
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import MembersList from './pages/MembersList'
-import MemberProfile from './pages/MemberProfile'
-import Payments from './pages/Payments'
-import Projects from './pages/Projects'
-import JobBoard from './pages/JobBoard'
-import Community from './pages/Community'
-import Leaders from './pages/Leaders'
-import Resources from './pages/Resources'
-import Chapters from './pages/Chapters'
+// Main Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+// Membership
+import Register from "./pages/Register";
+import MembersList from "./pages/MembersList";
+import MemberProfile from "./pages/MemberProfile";
+import Chapters from "./pages/Chapters";
+
+// Dashboard
+import Dashboard from "./pages/Dashboard";
+
+// Community
+import Community from "./pages/Community";
+
+// Payments
+import Payments from "./pages/Payments";
+
+// About Pages
+import Leaders from "./pages/Leaders";
+import Projects from "./pages/Projects";
+import Resources from "./pages/Resources";
+
+// Extras
+import JobBoard from "./pages/JobBoard";
 
 export default function App() {
   return (
     <>
       <Header />
 
-      <div className="container">
+      <main className="container">
         <Routes>
           {/* Home */}
           <Route path="/" element={<Home />} />
 
-          {/* Registration */}
+          {/* About */}
+          <Route path="/about" element={<About />} />
+          <Route path="/leaders" element={<Leaders />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resources" element={<Resources />} />
+
+          {/* Membership */}
           <Route path="/register" element={<Register />} />
+          <Route path="/members" element={<MembersList />} />
+          <Route path="/members/:id" element={<MemberProfile />} />
+          <Route path="/chapters" element={<Chapters />} />
 
           {/* Dashboard */}
           <Route path="/dashboard" element={<Dashboard />} />
 
-          {/* Members */}
-          <Route path="/members" element={<MembersList />} />
-          <Route path="/members/:id" element={<MemberProfile />} />
-
-          {/* Chapters */}
-          <Route path="/chapters" element={<Chapters />} />
+          {/* Payments */}
+          <Route path="/payments" element={<Payments />} />
 
           {/* Community */}
           <Route path="/community" element={<Community />} />
 
-          {/* Payments */}
-          <Route path="/payments" element={<Payments />} />
-
-          {/* Leaders */}
-          <Route path="/leaders" element={<Leaders />} />
-
-          {/* Projects */}
-          <Route path="/projects" element={<Projects />} />
-
           {/* Jobs */}
           <Route path="/jobs" element={<JobBoard />} />
 
-          {/* Resources */}
-          <Route path="/resources" element={<Resources />} />
+          {/* Contact */}
+          <Route path="/contact" element={<Contact />} />
 
           {/* 404 */}
           <Route
             path="*"
             element={
-              <div style={{ padding: '2rem', textAlign: 'center' }}>
-                <h2>404 - Page Not Found</h2>
-                <p>The page you are looking for does not exist.</p>
+              <div
+                style={{
+                  padding: "4rem 1rem",
+                  textAlign: "center",
+                }}
+              >
+                <h1>404</h1>
+                <h2>Page Not Found</h2>
+                <p>
+                  Sorry, the page you're looking for doesn't exist.
+                </p>
               </div>
             }
           />
         </Routes>
-      </div>
+      </main>
     </>
-  )
+  );
 }
